d2769d696dc262b5660882af2bcf99a5d49e685e
Kok Foong Lee <kfl31@cam.ac.uk>
2025-08-18 10:41:38 +0800

    1-first-branch: improve user facing name

--- stack-data-uploader/inputs/data/sgpostcode/sql/add_iri.sql
+++ stack-data-uploader/inputs/data/sgpostcode/sql/add_iri.sql
@@ -1,4 +1,7 @@
-ALTER TABLE sgpostcode ADD COLUMN iri TEXT;
+ALTER TABLE sgpostcode
+ADD COLUMN iri TEXT,
+ADD COLUMN name TEXT;
 
 UPDATE sgpostcode
-SET iri = 'https://www.theworldavatar.com/kg/location/' || ogc_fid;
+SET iri  = 'https://www.theworldavatar.com/kg/location/' || ogc_fid,
+    name = 'Postal code: ' || postal_code;
