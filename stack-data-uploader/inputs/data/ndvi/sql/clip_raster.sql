0d1a937f66750f6a72e05d373862a046ba182e17
Kok Foong Lee <kfl31@cam.ac.uk>
2026-03-12 15:28:50 +0800

    9-time-filtering: updated clip_raster.sql

--- stack-data-uploader/inputs/data/ndvi/sql/clip_raster.sql
+++ stack-data-uploader/inputs/data/ndvi/sql/clip_raster.sql
@@ -6,18 +6,18 @@ CREATE TABLE ndvi_raster_with_properties (
     raster_id int REFERENCES ndvi_raster(rid) ON DELETE CASCADE,
     year int,
     formal_green int,
-    public_access_closest_buffer int,
+    public_access int,
     area double precision,
     rast raster
 );
 
-INSERT INTO ndvi_raster_with_properties(poly_id, raster_id, year, formal_green, public_access_closest_buffer, area, rast)
+INSERT INTO ndvi_raster_with_properties(poly_id, raster_id, year, formal_green, public_access, area, rast)
 SELECT 
     p.ogc_fid AS poly_id,
     r.rid AS raster_id,
     r.year,
-    p.formal_green,
-    p.public_access_closest_buffer,
+    p.formal_green_final_value_imputed AS formal_green,
+    p.public_access_final_value_imputed AS public_access,
     r.area,
     ST_Clip(r.rast, p.wkb_geometry) AS rast
 FROM ndvi_raster r
@@ -26,6 +26,6 @@ ON p.year=r.year
 AND ST_Intersects(r.rast, p.wkb_geometry);
 
 CREATE INDEX ndvi_raster_clipped_year_idx ON ndvi_raster_with_properties(year);
-CREATE INDEX ndvi_raster_clipped_public_idx ON ndvi_raster_with_properties(public_access_closest_buffer);
+CREATE INDEX ndvi_raster_clipped_public_idx ON ndvi_raster_with_properties(public_access);
 CREATE INDEX ndvi_raster_clipped_formal_idx ON ndvi_raster_with_properties(formal_green);
 CREATE INDEX ndvi_raster_clipped_gix ON ndvi_raster_with_properties USING GIST(ST_ConvexHull(rast));
