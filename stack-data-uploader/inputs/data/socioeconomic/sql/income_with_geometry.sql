fba9edc6fa7661c88af6c8ef75a1ac6c0e569ac9
Kok Foong Lee <kfl31@cam.ac.uk>
2026-05-22 10:31:23 +0800

    9-time-filtering: reverted income_with_geometry.sql

--- stack-data-uploader/inputs/data/socioeconomic/sql/income_with_geometry.sql
+++ stack-data-uploader/inputs/data/socioeconomic/sql/income_with_geometry.sql
@@ -1,13 +1,7 @@
 CREATE TABLE IF NOT EXISTS income_with_geometry AS (
-SELECT
-    income."Number" AS planning_area,
-    (COALESCE("Below_1_000",0) + COALESCE("1_000_1_999",0) + COALESCE("2_000_2_999",0)) AS "3000",
-    (COALESCE("3_000_3_999",0) + COALESCE("4_000_4_999",0) + COALESCE("5_000_5_999",0)) AS "6000",
-    (COALESCE("6_000_6_999",0) + COALESCE("7_000_7_999",0) + COALESCE("8_000_8_999",0)) AS "9000",
-    (COALESCE("9_000_9_999",0) + COALESCE("10_000_10_999",0) + COALESCE("11_000_11_999",0)) AS "12000",
-    (COALESCE("12_000_12_999",0) + COALESCE("13_000_13_999",0) + COALESCE("14_000_14_999",0)) AS "15000",
-    (COALESCE("15_000_17_499",0) + COALESCE("17_500_19_999",0) + COALESCE("20_000andOver",0)) AS "others", 
-    wkb_geometry
+SELECT income."Number" AS planning_area, income."Below_1_000", income."1_000_1_999", income."2_000_2_999", income."3_000_3_999", income."4_000_4_999", income."5_000_5_999", income."6_000_6_999",
+income."7_000_7_999", income."8_000_8_999", income."9_000_9_999", income."10_000_10_999", income."11_000_11_999", income."12_000_12_999", income."13_000_13_999", income."14_000_14_999", 
+income."15_000_17_499", income."17_500_19_999", income."20_000andOver", wkb_geometry
 FROM income
 JOIN planning_area ON LOWER(income."Number")=LOWER(planning_area."PLN_AREA_N")
 );
--- stack-data-uploader/inputs/data/socioeconomic/sql/income_with_geometry_original.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS income_with_geometry_original AS (
-SELECT income."Number" AS planning_area, income."Below_1_000", income."1_000_1_999", income."2_000_2_999", income."3_000_3_999", income."4_000_4_999", income."5_000_5_999", income."6_000_6_999",
-income."7_000_7_999", income."8_000_8_999", income."9_000_9_999", income."10_000_10_999", income."11_000_11_999", income."12_000_12_999", income."13_000_13_999", income."14_000_14_999", 
-income."15_000_17_499", income."17_500_19_999", income."20_000andOver", wkb_geometry
-FROM income
-JOIN planning_area ON LOWER(income."Number")=LOWER(planning_area."PLN_AREA_N")
-);
-
-CREATE INDEX IF NOT EXISTS income_with_geometry_original_idx ON income_with_geometry_original USING GIST (wkb_geometry);
