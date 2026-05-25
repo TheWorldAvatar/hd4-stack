3f584cc476a2ab177d7c31881953c84b7c1413ba
Kok Foong Lee <kfl31@cam.ac.uk>
2026-05-25 16:18:34 +0800

    9-time-filtering: added script for batch release

--- miscellaneous scripts/batch_release.py
+++ miscellaneous scripts/batch_release.py
@@ -1,10 +1,8 @@
 import pandas as pd
-from shapely import Point
 from collections import defaultdict
 import csv
 from pathlib import Path
 import json
-from itertools import product
 
 # combines multiple files into a single file
 path = Path('input/batch_release_input.json')
@@ -14,8 +12,19 @@ if not path.exists():
 with path.open("r", encoding="utf-8") as f:
     inputs = json.load(f)
 
-files = inputs['input_files']
+files = list(Path(inputs['folder']).glob("*.csv"))
 
+headers = set()
+# collect headers, and make sure each file has a postal_code header
+for file in files:
+    data = pd.read_csv(file, dtype={'postal_code': 'string'})
+    cols = list(data.columns)
+
+    if 'postal_code' not in cols:
+        raise Exception('postal_code must be present')
+    headers.update(cols)
+
+postal_codes_not_equal = set()
 postal_code_to_header_to_value = defaultdict(dict)
 for file in files:
     data = pd.read_csv(file, dtype={'postal_code': 'string'})
@@ -31,22 +40,30 @@ for file in files:
 
         for key, item in row_data.items():
             if key in postal_code_to_header_to_value[postal_code]:
-                if postal_code_to_header_to_value[postal_code][key] != item:
-                    raise Exception('not equal?')
+                if key == 'GAD1_latlon_postcode' and postal_code_to_header_to_value[postal_code][key] != item:
+                    postal_codes_not_equal.add(postal_code)
             postal_code_to_header_to_value[postal_code][key] = item
 
+if len(postal_codes_not_equal) > 0:
+    print(postal_codes_not_equal)
+
 data_for_csv = []
+result_headers_set = set()
+
 for key, item in postal_code_to_header_to_value.items():
     row_for_csv = {}
     row_for_csv['postal_code'] = key
 
     for header, value in item.items():
         row_for_csv[header] = value
+        result_headers_set.add(header)
 
     data_for_csv.append(row_for_csv)
 
 with open(inputs['output_file'], 'w', newline="", encoding="utf-8") as f:
-    writer = csv.DictWriter(f, fieldnames=data_for_csv[0].keys())
+    headers = ['postal_code']
+    headers.extend(result_headers_set)
+    writer = csv.DictWriter(f, fieldnames=headers)
     writer.writeheader()
     for row in data_for_csv:
         writer.writerow(row)
